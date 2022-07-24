b47c431af1c2baf1adbf6d460172b9099cc02453
RIYAZ AHMAD <riyazahmad.online@gmail.com>
2022-07-25 01:30:41 +0530

    cart_data

--- db.js
+++ db.js
@@ -3,6 +3,7 @@
 // const brands = require("./data/brands");
 // const itemTypes = require("./data/itemTypes");
 const products=require("./data/db.json")
+const cart=require("./data/cart.json")
       
 module.exports = {
   // items,
@@ -10,5 +11,6 @@ module.exports = {
   // brands,
   // itemTypes
   products
+  ,cart
 }
 
