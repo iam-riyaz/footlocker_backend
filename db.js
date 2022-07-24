f3670352daa4a4b36fe22159e9be2f23dc8fbed9
RIYAZ AHMAD <riyazahmad.online@gmail.com>
2022-07-24 18:20:13 +0530

    testing

--- db.js
+++ db.js
@@ -3,7 +3,7 @@
 // const brands = require("./data/brands");
 // const itemTypes = require("./data/itemTypes");
 const products=require("./data/db.json")
-
+      
 module.exports = {
   // items,
   // tags,
